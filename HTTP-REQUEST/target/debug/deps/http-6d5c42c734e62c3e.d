b69afbba1591b5e9e609d6a2cccb6489ec2daea1
mostvivat <pokemost@gmail.com>
2023-03-03 04:57:53 +0700

    lab7

---
+++ HTTP-REQUEST/target/debug/deps/http-6d5c42c734e62c3e.d
@@ -0,0 +1,5 @@
+C:\Users\pokem\OneDrive\Documents\Mobile-and-Cloud\kmutnb-cn-vivat-3\lab07-2023-02-24\HTTP-REQUEST\target\debug\deps\http-6d5c42c734e62c3e.exe: src\main.rs
+
+C:\Users\pokem\OneDrive\Documents\Mobile-and-Cloud\kmutnb-cn-vivat-3\lab07-2023-02-24\HTTP-REQUEST\target\debug\deps\http-6d5c42c734e62c3e.d: src\main.rs
+
+src\main.rs:
---
+++ HTTP-REQUEST/target/debug/deps/http.d
@@ -0,0 +1,5 @@
+C:\Users\pokem\OneDrive\Documents\Mobile-and-Cloud\kmutnb-cn-vivat-3\lab07-2023-02-24\HTTP-REQUEST\target\debug\deps\http.exe: src\main.rs
+
+C:\Users\pokem\OneDrive\Documents\Mobile-and-Cloud\kmutnb-cn-vivat-3\lab07-2023-02-24\HTTP-REQUEST\target\debug\deps\http.d: src\main.rs
+
+src\main.rs:
